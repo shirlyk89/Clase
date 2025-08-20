24e3b757764beaca0eeafb9e09154fb712f42252
shirlyk89 <226595573+shirlyk89@users.noreply.github.com>
2025-08-20 16:45:21 -0500

    imc exercise

--- documento.py
+++ documento.py
@@ -1,1 +1,15 @@
-print("Hola mundo")
+import os
+os.system("cls")
+
+peso=float(input("Ingrese su peso: "))
+altura=float(input("Ingrese su altura: "))
+imc=peso/(altura**2) #imc es el indice de masa corporal
+
+if imc < 18.5:
+    print(f"Usted tiene bajo peso y su imc es: {imc:.2f}")
+elif imc >= 18.5 and imc < 24.9:
+    print(f"Usted tiene un peso normal y su imc es: {imc:.2f}")
+elif imc >= 24.9 and imc < 29.9:
+    print(f"Usted tiene sobrepeso y su imc es: {imc:.2f}")
+else:
+    print(f"Usted tiene obesidad y su imc es: {imc:.2f}")
