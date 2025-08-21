ad9471d9c521630c463990065258ae1f8d1ad3cb
shirlyk89 <226595573+shirlyk89@users.noreply.github.com>
2025-08-21 17:46:22 -0500

    empresario_creater

---
+++ bucles.py
@@ -0,0 +1,10 @@
+"""numero=int(input("introduce un numero: "))
+for i in range(1,numero+1, 2):
+    print(i)"""
+
+"""palabra=["hola","mundo"]
+for i in palabra:
+    print(i)"""""""
+
+    
+    isactive=true
